19c9223273367693ae85d552a8c24219527b6f99
jsango2 <jure.sango@gmail.com>
2022-03-07 22:37:14 +0100

    commit

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -15,7 +15,7 @@ const Form = ({ title }) => {
         <script src="https://webforms.pipedrive.com/f/loader"></script>
       </Helmet>
       <FormWrap>
-        {/* <FormTitle>
+        <FormTitle>
           Ispuni podatke danas - Udvostruči prihode, iskorisiti prilike.
         </FormTitle>
 
@@ -35,21 +35,18 @@ const Form = ({ title }) => {
               required
             />
             <input type="email" placeholder="Email" name="email" required />
-         
             <input
               type="text"
               placeholder="Ime objekta"
               name="ime objekta"
               required
             />
-          
             <input
               type="tel"
               placeholder="Broj telefona"
               name="broj telefona"
               required
             />{" "}
-          
             <label className="container">
               Želim primati Rentlio newsletter za hotele
               <input type="checkbox" defaultChecked="checked" />
@@ -61,11 +58,11 @@ const Form = ({ title }) => {
               type="submit"
             />
           </form>
-        </FieldsWrap> */}
-        <WrapPipe
+        </FieldsWrap>
+        {/* <WrapPipe
           className="pipedriveWebForms"
           data-pd-webforms="https://webforms.pipedrive.com/f/JJX6B6Lc6P3UBwqFymiXk3rK8vxUcKI88zr6IyurUq7FDYr4tjVWYHJUOZUSeTV"
-        ></WrapPipe>
+        ></WrapPipe> */}
       </FormWrap>
       <FormBehind />
     </>
---
+++ src/pages/success.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const success = () => {
+  return <div>success! Form sent</div>
+}
+
+export default success
